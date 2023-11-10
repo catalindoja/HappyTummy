f4c723b4110375ee8f1f7f0d00c412b0437cb036
catalindoja <37113386+catalindoja@users.noreply.github.com>
2023-11-10 16:16:38 +0100

    trying to do the menu of the mobileapp

--- mobileapp/src/App.js
+++ mobileapp/src/App.js
@@ -3,12 +3,44 @@ import logo from './logo.svg';
 import './App.css';
 import Counter from './components/Counter';
 import Home from './pages/Home';
+import Menu from './components/Menu';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <>
+
+      <Outlet />
+      <Counter />
+      <Menu />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      }
+    ]
+  },
+  
+]);
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <Counter /> 
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
     </div>
   );
 }
--- mobileapp/src/pages/Home.jsx
+++ mobileapp/src/pages/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
                 </div>
             </div>
         </div>
-
+    
         
        
     )
