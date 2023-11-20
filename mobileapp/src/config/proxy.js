7b482f2ea1e265cc3e2154c04f07b01227944ba7
catalindoja <37113386+catalindoja@users.noreply.github.com>
2023-11-20 14:19:50 +0100

    mistakingly put a "/" after the proxy url

--- mobileapp/src/config/proxy.js
+++ mobileapp/src/config/proxy.js
@@ -1,2 +1,2 @@
 // apiConstants.js
-export const BACKEND_API_URL = 'https://happytummy-backend-production.up.railway.app/';
+export const BACKEND_API_URL = 'https://happytummy-backend-production.up.railway.app';
