0ecb2a91439e3931bff74067fd06cec4b258bfea
catalindoja <37113386+catalindoja@users.noreply.github.com>
2023-12-12 20:11:46 +0100

    Done the comments to recipes. Also done the reply comments part. Maybe the edit comment part would be needed.

--- frontend/webpage/src/pages/SingleRecipe.jsx
+++ frontend/webpage/src/pages/SingleRecipe.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../context/authContext";
@@ -6,10 +6,12 @@ import Edit from "../img/edit.png";
 import Delete from "../img/delete.png";
 import ProfilePicture from "../img/profile.png";
 import Heart from "../img/heart.png";
+import Reply from "../img/reply.png";
 import Arrow from "../img/arrow.png";
 import Menu from "../components/MenuRecipes";
 import axios from "axios";
 import DOMPurify from "dompurify";
+import ReactQuill from "react-quill";
 
 // Create the SingleRecipe component
 const SingleRecipe = () => {
@@ -24,14 +26,117 @@ const SingleRecipe = () => {
     const idCurrent = currentUser.id;
     const usernameCurrent = currentUser.username;
 
+    // - comments: an array that contains the details of the comments of the post
+    // - userComments: an object that contains the details of the users who wrote the comments
+    const [comments, setComments] = useState([]);
+    const [userComments, setUserComments] = useState({});
+
     // Owner of the post
     const idOwner = post.iduser;
     const [userOwner, setOwner] = useState("");
+
+    // Write new comment
+    const state = useLocation().state;
+    const [value, setValue] = useState(state?.newComment || "");
+    
+    // Post comment
+    const handleClick = async (e) => {
+        e.preventDefault();
+        try {
+        const productResponse = await axios.post(`/commentrecipes/`, {
+            iduser: idCurrent,
+            idproduct: postId,
+            content: value,
+            likes: 0
+        });
+
+        window.location.reload();
+
+        } catch (err) {
+        console.log(err);
+        }
+    }
+
+    // Add state variables for handling reply functionality
+    const [replyingTo, setReplyingTo] = useState(null); // To store the ID of the comment being replied to
+    const [replyContent, setReplyContent] = useState(""); // To store the content of the reply
+    const [isReplyVisible, setIsReplyVisible] = useState(false);
+
+    // Inside your functional component
+    const replyContainerRef = useRef(null);
+
+    // Use useEffect to trigger the scroll when isReplyVisible changes
+    useEffect(() => {
+        if (isReplyVisible && replyContainerRef.current) {
+        replyContainerRef.current.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        });
+        }
+    }, [isReplyVisible]);
+
+    // Function to handle opening the reply pop-up
+    const handleReply = (commentId, username) => {
+        setReplyingTo(commentId);
+        setIsReplyVisible(true);
+    };
+
+    // Function to handle submitting the reply
+    const submitReply = async () => {
+        try {
+        // Send the reply to the backend
+        await axios.post(`/commentrecipes/`, {
+            iduser: idCurrent,
+            idrecipe: postId,
+            content: replyContent,
+            likes: 0, // Assuming initial likes count is 0
+            idparent: replyingTo, // Add the ID of the comment being replied to
+        });
+        // Close the modal/pop-up and reset state variables
+        setIsReplyVisible(false);
+        setReplyingTo(null);
+        setReplyContent("");
+        // Refresh comments or update state to include the new reply
+
+        window.location.reload();
+        } catch (err) {
+        console.log(err);
+        }
+    };
+
+    const closeReplyModal = () => {
+        setIsReplyVisible(false);
+        setReplyingTo(null);
+        setReplyContent("");
+    };
 
     // Obtaining the recipe
     useEffect(() => {
         const fetchData = async () => {
             try {
+                // Obtain comments
+                const res2 = await axios.get(`/commentrecipes/`);
+                console.log(res2.data)
+                const filteredComments = res2.data.filter((comment) => comment.idrecipe == postId);
+                console.log(postId)
+                setComments(filteredComments);
+                console.log(filteredComments)
+
+                // Obtain users who wrote the comments
+                const userPromises = filteredComments.map((comment) => axios.get(`/users/${comment.iduser}`));
+                const userResponses = await Promise.all(userPromises);
+
+                // Create an object that contains the details of the users who wrote the comments
+                const userCommentsData = {};
+                userResponses.forEach((response, index) => {
+                const comment = filteredComments[index];
+                userCommentsData[comment.id] = response.data;
+                });
+
+                // Set the state variable 'userComments' to the object that contains the details of the users who wrote the comments
+                setUserComments(userCommentsData);
+
+
                 // Recepie
                 const res = await axios.get(`/recipes/${postId}`);
                 setPosts(res.data);
@@ -127,8 +232,83 @@ const SingleRecipe = () => {
                             __html: DOMPurify.sanitize(post.description),
                         }}
                     ></p>
-                </div>
 
+                    <h3 className="comments-heading">Comments</h3>
+                    <ul className="comments-list">
+                    {comments.length === 0 ? (
+                        <p>No comments yet!</p>
+                    ) : (
+                        comments.map(comment => {
+                        const parentComment = comments.find(c => c.id === comment.idparent);
+                        
+                        return (
+                            <li key={comment.id} className="comment">
+                            <div className="comment-content">
+                                <div className="user-info">
+                                <img src={ProfilePicture} alt="Profile Picture" className="user-image" />
+                                <span className="username">
+                                    {userComments[comment.id] ? userComments[comment.id].username : "Unknown"}
+                                    {comment.idparent && parentComment && (
+                                    <> replied to {userComments[parentComment.id]?.username}</>
+                                    )}
+                                </span>
+                                </div>
+                                <p
+                                dangerouslySetInnerHTML={{
+                                    __html: DOMPurify.sanitize(comment.content)
+                                }}
+                                ></p>
+                            </div>
+                            <div className="comment-likes">
+                                <button onClick={() => handleReply(comment.id, userComments[comment.id]?.username)}>
+                                <img src={Reply} alt="Reply Icon" className="heart-icon" />
+                                </button>
+                                <button onClick={handleLikeClick}>
+                                <img src={Heart} alt="Heart Icon" className="heart-icon" />
+                                </button>
+                                <span className="likes-count">{comment.likes}</span>
+                            </div>
+                            </li>
+                        );
+                        }
+                        ))}
+                    </ul>
+
+                    {isReplyVisible && (
+                    <div ref={replyContainerRef} className="reply-container">
+                        <div className="reply-header">
+                        <span>Replying to: {userComments[replyingTo]?.username}</span>
+                        </div>
+                        <div className="reply-body">
+                        <textarea
+                            value={replyContent}
+                            onChange={(e) => setReplyContent(e.target.value)}
+                            placeholder="Write your reply..."
+                            className="reply-textarea"
+                        />
+                        <div className="reply-buttons">
+                            <button onClick={submitReply} className="submit-reply-button">Submit</button>
+                            <button onClick={closeReplyModal} className="cancel-reply-button">Cancel</button>
+                        </div>
+                        </div>
+                    </div>
+                    )}
+
+                    <h3 className="write-comment-heading">Write a new comment!</h3>
+                    <div className="editorContainer">
+                    <ReactQuill
+                        placeholder="New comment"
+                        className="editor"
+                        theme="snow"
+                        value={value}
+                        onChange={setValue}
+                    />
+                    </div>
+
+                    <div className="buttons">
+                    <button onClick={handleClick}> Publish</button>
+                    </div>
+                </div>
                 {<Menu />}
             </div>
         </div>
